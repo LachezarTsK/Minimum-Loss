5e54b0e2078937f8cd3d6527ca30cf782811b264
Lachezar Kutsarov <lkutsarov@gmail.com>
2018-09-28 20:25:12 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,10 +1,13 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
 
+	/**
+	 * Applying HashMap so that each year can be accessed through the price with
+	 * O(1) time.
+	 */
 	private static Map<Long, Integer> pricesPerYears = new HashMap<Long, Integer>();
 
 	public static long minimumLoss(long[] price) {
